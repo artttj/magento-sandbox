225993b8ccb55124a172c90b55d568f1928b6d18
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 11:57:22 +0600

    Added: registry entity

---
+++ app/code/local/A2/Giftregistry/Model/Entity.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* 
+*/
+class A2_Giftregistry_Model_Entity extends Mage_Core_Model_Abstract
+{
+    
+    function __construct()
+    {
+        $this->_init('a2_giftregistry/entity');
+        parent::_construct();
+    }
+}
