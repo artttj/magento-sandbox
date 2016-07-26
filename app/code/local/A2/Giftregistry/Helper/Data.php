fa53e575573639c4b5d376b4bdff04ea5e99976d
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 11:28:21 +0600

    Added: gift registry model

---
+++ app/code/local/A2/Giftregistry/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* 
+*/
+class A2_Giftregistry_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    
+    function __construct(argument)
+    {
+        # code...
+    }
+}
---
+++ app/code/local/A2/Giftregistry/Model/Type.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* 
+*/
+class A2_Giftregistry_Model_Type extends Mage_Core_Model_Abstract
+{
+    
+    function __construct()
+    {
+        $this->_init('a2_giftregistry/type');
+        parent::_construct();
+    }
+}
