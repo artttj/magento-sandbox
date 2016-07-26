988e51d527c7ceaec3226460e2f1bb4b582c830b
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 12:14:41 +0600

    Fixed typo

--- app/code/local/A2/Giftregistry/Model/Mysql4/Item.php
+++ app/code/local/A2/Giftregistry/Model/Mysql4/Item.php
@@ -8,6 +8,6 @@ class A2_Giftregistry_Model_Mysql4_Item extends Mage_Core_Model_Mysql4_Abstract
     
     public function _construct()
     {
-        $this->_init('a2_giftregistry/item', 'item_idAd');
+        $this->_init('a2_giftregistry/item', 'item_id');
     }
 }
