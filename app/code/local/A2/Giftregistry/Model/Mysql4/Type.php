8b2aa6c92b15cc8496c9861205bc40fea0ed8819
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 11:52:07 +0600

    Changed function declaration

--- app/code/local/A2/Giftregistry/Model/Mysql4/Type.php
+++ app/code/local/A2/Giftregistry/Model/Mysql4/Type.php
@@ -6,7 +6,7 @@
 class A2_Giftregistry_Model_Mysql4_Type extends Mage_Core_Model_Mysql4_Abstract
 {
     
-    function _construct()
+    public function _construct()
     {
         $this->_init('a2_giftregistry/type', 'type_id');
     }
