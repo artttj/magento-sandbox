58f989974fcbe01ba0a933356c0c99990d22fd42
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 11:59:34 +0600

    Fixed construct

--- app/code/local/A2/Giftregistry/Model/Entity.php
+++ app/code/local/A2/Giftregistry/Model/Entity.php
@@ -5,7 +5,7 @@
 class A2_Giftregistry_Model_Entity extends Mage_Core_Model_Abstract
 {
     
-    function __construct()
+    public function __construct()
     {
         $this->_init('a2_giftregistry/entity');
         parent::_construct();
