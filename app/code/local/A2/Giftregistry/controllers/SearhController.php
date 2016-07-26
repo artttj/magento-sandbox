4cf9e43b28343243607e2f469eb4d0cd739cde1d
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 16:10:02 +0600

    Added: results action

--- app/code/local/A2/Giftregistry/controllers/SearhController.php
+++ app/code/local/A2/Giftregistry/controllers/SearhController.php
@@ -12,7 +12,21 @@ class A2_Giftregistry_SearchController extends Mage_Core_Controller_Front_Action
 
     public function resultsAction() {
         $this->loadLayout();
+        if ($searchParams = $this->getRequest()->getParam('search_params')) {
+            $results = Mage::getModel('a2_giftregistry/entity')->getCollection();
+            if ($searchParams['type']) {
+                $results->addFieldToFilter('type_id', $searchParams['type']);
+            }
+            if ($searchParams['date']) {
+                $results->addFieldToFilter('event_date', $searchParams['date']);
+            }
+            if ($searchParams['location']) {
+                $results->addFieldToFilter('event_location', $searchParams['location']);
+            }
+            $this->getLayout()->getBlock('a2_giftregistry.search.results')
+                ->setResults($results);
+        }
         $this->renderLayout();
-        return $this;        
-    }    
+        return $this;
+    }
 }
