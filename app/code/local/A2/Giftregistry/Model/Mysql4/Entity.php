0e7cb7327ece90f903fa9520f1c02f52daad70a7
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 12:14:58 +0600

    Added: entity model

---
+++ app/code/local/A2/Giftregistry/Model/Mysql4/Entity.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+* 
+*/
+class A2_Giftregistry_Model_Mysql4_Entity extends Mage_Core_Model_Mysql4_Abstract
+{
+    
+    public function _construct()
+    {
+        $this->_init('a2_giftregistry/entity', 'entity_id');
+    }
+}
