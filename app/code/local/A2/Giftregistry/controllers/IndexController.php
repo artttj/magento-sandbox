4d2316315ff7f0fa247a0d3515684a5919b396ca
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 14:44:48 +0600

    Resolved issue with the controller file name

---
+++ app/code/local/A2/Giftregistry/controllers/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* 
+*/
+class A2_Giftregistry_IndexController extends Mage_Core_Controller_Front_Action
+{
+    
+    public function indexAction()
+    {
+        echo 'Test controller :O';
+    }
+}
