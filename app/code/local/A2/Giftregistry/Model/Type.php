14a1dd422d9f3de380d0cdead44e22f595a557fb
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 15:01:34 +0600

    Fixed: type model

--- app/code/local/A2/Giftregistry/Model/Type.php
+++ app/code/local/A2/Giftregistry/Model/Type.php
@@ -5,7 +5,7 @@
 class A2_Giftregistry_Model_Type extends Mage_Core_Model_Abstract
 {
     
-    function __construct()
+    public function __construct()
     {
         $this->_init('a2_giftregistry/type');
         parent::_construct();
