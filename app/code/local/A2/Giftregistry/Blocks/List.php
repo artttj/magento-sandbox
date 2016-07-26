06dcea55a00d7bd9fdb04f2c4f4255eabb9b4d7c
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 16:41:21 +0600

    Added: list block

---
+++ app/code/local/A2/Giftregistry/Blocks/List.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* 
+*/
+class A2_Giftregistry_Block_List extends Mage_Core_Block_Template
+{
+    
+    public function getCustomerRegistries()
+    {
+        $collection = null;
+        $currentCustomer = Mage::getSingleton('customer/session')->getCustomer();
+        if ($currentCustomer) {
+            $collection = Mage::getModel('a2_giftregistry/entity')->getCollection()
+                ->addFieldTOFilter('customer_id', $currentCustomer->getId());
+        }
+        return $collection;
+    }
+}
