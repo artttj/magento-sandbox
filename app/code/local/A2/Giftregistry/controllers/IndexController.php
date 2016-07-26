1fc0190a8ac716bc9d9b323b6e3d9435d66b56dd
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 14:51:52 +0600

    Added: some actions

--- app/code/local/A2/Giftregistry/controllers/IndexController.php
+++ app/code/local/A2/Giftregistry/controllers/IndexController.php
@@ -1,13 +1,45 @@
 <?php
 /**
-* 
+*
 */
 class A2_Giftregistry_IndexController extends Mage_Core_Controller_Front_Action
 {
-    
+
     public function indexAction()
     {
         $this->loadLayout();
         $this->renderLayout();
+        return $this;
     }
+
+    public function deleteAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+        return $this;
+    }
+    public function newAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+        return $this;
+    }
+    public function editAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+        return $this;
+    }
+    public function newPostAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+        return $this;
+    }
+    public function editPostAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+        return $this;
+    }       
 }
