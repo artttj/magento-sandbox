9668b5582bce14ce9e73eaad25d67bfff885d474
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-26 16:17:24 +0600

    Added: view controller

---
+++ app/code/local/A2/Giftregistry/controllers/ViewController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+*
+*/
+class A2_Giftregistry_ViewController extends Mage_Core_Controller_Front_Action
+{
+    public function viewAction() 
+    {
+        $registryId = $this->getRequest()->getParam('registry_id');
+        if ($registryId) {
+            $entity = Mage::getModel('a2_giftregistry/entity');
+            if ($registryId) {
+                $entity = Mage::getModel('a2_giftregistry/entity');
+                if ($entity->load($registryId)) {
+                    Mage::register('loaded_registry', $entity);
+                    $this->loadLayout();
+                    $this->_initLayoutMessages('customer/session');
+                    $this->renderLayout();
+                    return $this;
+                } else {
+                    $this->_forward('noroute');
+                    return $this;
+                }
+            }
+        }
+    }
+}
