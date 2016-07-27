b6a09b1df9ccc9c9126be296d3521004572bb209
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-27 17:28:29 +0600

    Added: isItemInRegistry helper

--- app/code/local/A2/Giftregistry/Helper/Data.php
+++ app/code/local/A2/Giftregistry/Helper/Data.php
@@ -8,6 +8,14 @@ class A2_Giftregistry_Helper_Data extends Mage_Core_Helper_Abstract {
         return $collection;
     }
 
+    public function isItemInRegistry($itemId, $registryId)
+    {
+        return $itemCollection = Mage::getModel('a2_giftregistry/item')->getCollection()
+            ->addFieldToFilter('product_id', $itemId)
+            ->addFieldToFilter('registry_id', $registryId)
+            ->count();
+    }
+
     public function isRegistryOwner($registryCustomerId)
     {
         $currentCustomer = Mage::getSingleton('customer/session')->getCustomer();
