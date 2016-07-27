18f6d2f90b771e6a82f6e4153bccdbf1186d6469
Artyom Yagovdik <yagovdik@a2design.biz>
2016-07-27 11:02:13 +0600

    Added: helper methods

--- app/code/local/A2/Giftregistry/Helper/Data.php
+++ app/code/local/A2/Giftregistry/Helper/Data.php
@@ -1,12 +1,20 @@
 <?php
-/**
-* 
-*/
-class A2_Giftregistry_Helper_Data extends Mage_Core_Helper_Abstract
-{
-    
-    function __construct()
+
+class A2_Giftregistry_Helper_Data extends Mage_Core_Helper_Abstract {
+
+    public function getEventTypes()
     {
-        # code...
+        $collection = Mage::getModel('a2_giftregistry/type')->getCollection();
+        return $collection;
     }
+
+    public function isRegistryOwner($registryCustomerId)
+    {
+        $currentCustomer = Mage::getSingleton('customer/session')->getCustomer();
+        if($currentCustomer && $currentCustomer->getId() == $registryCustomerId) {
+            return true;
+        }
+        return false;
+    }
+
 }
