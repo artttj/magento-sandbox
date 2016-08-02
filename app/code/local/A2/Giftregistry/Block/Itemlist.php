6131c3154c79f60e9ae7b3774f4cd56ecd9bb9e1
Artyom Yagovdik <yagovdik@a2design.biz>
2016-08-02 15:18:32 +0600

    Fixed: bug with the product collection

--- app/code/local/A2/Giftregistry/Block/Itemlist.php
+++ app/code/local/A2/Giftregistry/Block/Itemlist.php
@@ -11,44 +11,14 @@ class A2_Giftregistry_Block_Itemlist extends Mage_Catalog_Block_Product_List
         foreach ($itemCollection as $item) {
             $productIds[] = $item->product_id;
         }
-        if (is_null($this->_productCollection)) {
-            $layer = $this->getLayer();
-            /* @var $layer Mage_Catalog_Model_Layer */
-            if ($this->getShowRootCategory()) {
-                $this->setCategoryId(Mage::app()->getStore()->getRootCategoryId());
-            }
 
-            // if this is a product view page
-            if (Mage::registry('product')) {
-                // get collection of categories this product is associated with
-                $categories = Mage::registry('product')->getCategoryCollection()
-                    ->setPage(1, 1)
-                    ->load();
-                // if the product is associated with any category
-                if ($categories->count()) {
-                    // show products from this category
-                    $this->setCategoryId(current($categories->getIterator()));
-                }
-            }
 
-            $origCategory = null;
-            if ($this->getCategoryId()) {
-                $category = Mage::getModel('catalog/category')->load($this->getCategoryId());
-                if ($category->getId()) {
-                    $origCategory = $layer->getCurrentCategory();
-                    $layer->setCurrentCategory($category);
-                    $this->addModelTags($category);
-                }
-            }
-            $this->_productCollection = $layer->getProductCollection()
-                ->addAttributeToFilter('entity_id', array('in' => $productIds));
-
-            $this->prepareSortableFieldsByCategory($layer->getCurrentCategory());
-
-            if ($origCategory) {
-                $layer->setCurrentCategory($origCategory);
-            }
-        }
+        $this->_productCollection = Mage::getModel('catalog/product')->getCollection()
+            ->addAttributeToSelect(Mage::getSingleton('catalog/config')->getProductAttributes())
+            ->addMinimalPrice()
+            ->addFinalPrice()
+            ->addTaxPercents()
+            ->addAttributeToFilter('entity_id', array('in' => $productIds));
 
         return $this->_productCollection;
     }
