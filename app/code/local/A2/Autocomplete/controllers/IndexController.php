b2713e7c76ff72e1fe1239080df927cb1d709f44
Artyom Yagovdik <yagovdik@a2design.biz>
2016-08-05 12:56:55 +0600

    Added: search method

--- app/code/local/A2/Autocomplete/controllers/IndexController.php
+++ app/code/local/A2/Autocomplete/controllers/IndexController.php
@@ -10,6 +10,39 @@ class A2_Autocomplete_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
+        $searchedProducts = array();
+        $searchedProduct = array(
+            'link' => '',
+            'image' => '',
+            'price' => '',
+        );
+        $links = array();
+        $q = $this->getRequest()->getParam('q');
+        if (empty($q)) {
+            return;
+        }
+        // Code to Search Product by $searchstring and get Product IDs
+        $product_collection = Mage::getResourceModel('catalog/product_collection')
+                  ->addAttributeToSelect('*')
+                  ->addAttributeToFilter('name', array('like' => '%'.$q.'%'))
+                  ->load();
 
+        foreach ($product_collection as $product) {
+            $productId = $product->getId();
+            $productEntity = Mage::getResourceSingleton('catalog/product');
+            $link = $productEntity
+                ->getAttributeRawValue($productId, 'url_path', Mage::app()->getStore());
+            $image = $productEntity
+                ->getAttributeRawValue($productId, 'small_image', Mage::app()->getStore());
+            $price = $productEntity
+                ->getAttributeRawValue($productId, 'price', Mage::app()->getStore());                
+            $searchedProduct['link'] = '/' . $link;
+            $searchedProduct['image'] = '/media/catalog/product/' . $image;
+            $searchedProduct['price'] = '/' . $price;
+            $searchedProducts[] = $searchedProduct;
+        }
+        //return array of product ids
+        echo $json = Mage::helper('core')->jsonEncode($searchedProducts);
+        return;
     }
 }
