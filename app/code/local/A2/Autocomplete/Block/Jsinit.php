b1d4f5b334b0234ef0c879cc69b6f1bb3ea38ec0
Artyom Yagovdik <yagovdik@a2design.biz>
2016-08-05 13:16:40 +0600

    Added: js script for products search

--- app/code/local/A2/Autocomplete/Block/Jsinit.php
+++ app/code/local/A2/Autocomplete/Block/Jsinit.php
@@ -6,10 +6,26 @@ class A2_Autocomplete_Block_Jsinit extends Mage_Core_Block_Text
     protected function _toHtml()
     {
         $html = '';
+
         $html .= 
             "<script type=\"text/javascript\">
             //<![CDATA[
-                console.log(1);
+                (function(){
+                    document.addEventListener(\"DOMContentLoaded\", function(event) { 
+                        var searchInput = document.querySelector('#search,form input[name=\"q\"]');
+                        searchInput.addEventListener('input', function(event){
+                            var q = event.target.value;
+                            new Ajax.Request('/autocomplete/index/index', {
+                                method: 'get',
+                                parameters: {
+                                    q: q
+                                },
+                                onSuccess: function(response) {
+                                }
+                            });
+                        });
+                    });
+                }());
             //]]>
         </script>
         ";
